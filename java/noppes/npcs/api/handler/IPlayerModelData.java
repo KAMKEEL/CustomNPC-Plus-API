75ef3578f62f894217e001b7259291b6a956834d
Luis C.M <42545778+c0508383@users.noreply.github.com>
2023-01-08 23:49:14 -0600

    IPlayerModelData: -Re-implemented some previously removed functions.

--- java/noppes/npcs/api/handler/IPlayerModelData.java
+++ java/noppes/npcs/api/handler/IPlayerModelData.java
@@ -8,13 +8,53 @@ public interface IPlayerModelData {
 
     IModelPart getPart(int part);
 
+    void setEnabled(boolean enabled);
+
+    boolean enabled();
+
     void setAnimated(boolean animated);
+
+    boolean isAnimated();
 
     void setInterpolated(boolean interpolate);
 
+    boolean isInterpolated();
+
     void setFullAngles(boolean fullAngles);
 
+    boolean fullAngles();
+
     void setAnimRate(float animRate);
+
+    float getAnimRate();
+
+    void setRotation(float rotationX, float rotationY, float rotationZ);
+
+    void setRotationX(float rotationX);
+
+    void setRotationY(float rotationY);
+
+    void setRotationZ(float rotationZ);
+
+    float getRotationX();
+
+    float getRotationY();
+
+    float getRotationZ();
+
+    void setRotationEnabled(boolean enabledX, boolean enabledY, boolean enabledZ);
+
+    void setRotationEnabledX(boolean enabled);
+
+    void setRotationEnabledY(boolean enabled);
+
+    void setRotationEnabledZ(boolean enabled);
+
+    boolean rotationEnabledX();
+
+    boolean rotationEnabledY();
+
+    boolean rotationEnabledZ();
 
     void allEnabled(boolean enabled);
 
