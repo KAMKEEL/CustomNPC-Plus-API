141ce1e170550468232f7f307c4b087bf9c171ab
Luis C.M <42545778+c0508383@users.noreply.github.com>
2023-03-17 03:04:25 -0500

    IModelData: -Added hidePart() comment

--- java/noppes/npcs/api/entity/data/IModelData.java
+++ java/noppes/npcs/api/entity/data/IModelData.java
@@ -2,6 +2,11 @@ package noppes.npcs.api.entity.data;
 
 public interface IModelData {
 
+    /**
+     * Hide Body Parts
+     * part: [0: Head, 1: Body, 2: Arms, 3: Legs]
+     * hide: [0: None, 1: Both, 2: Right, 3: Left], only values 0 and 1 used for head & body.
+     */
     void hidePart(int part, byte hide);
 
     int hidden(int part);
