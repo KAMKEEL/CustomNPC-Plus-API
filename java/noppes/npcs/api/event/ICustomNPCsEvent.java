f96bb398ff2179f4a7761740f7e959e6b0d22be8
Goodbird <goodbirdmail@yandex.ru>
2024-01-20 11:43:57 +0300

    Added an interface for the ScriptedCommandEvent

--- java/noppes/npcs/api/event/ICustomNPCsEvent.java
+++ java/noppes/npcs/api/event/ICustomNPCsEvent.java
@@ -2,6 +2,7 @@ package noppes.npcs.api.event;
 
 import cpw.mods.fml.common.eventhandler.Cancelable;
 import noppes.npcs.api.IPos;
+import noppes.npcs.api.IWorld;
 import noppes.npcs.api.handler.data.INaturalSpawn;
 
 public interface ICustomNPCsEvent {
@@ -23,5 +24,19 @@ public interface ICustomNPCsEvent {
         boolean liquidSpawnPassed();
 
         boolean airSpawnPassed();
+    }
+
+    interface ScriptedCommandEvent extends ICustomNPCsEvent {
+        IWorld getSenderWorld();
+
+        IPos getSenderPosition();
+
+        String getSenderName();
+
+        void setReplyMessage(String message);
+
+        int getId();
+
+        String[] getArgs();
     }
 }
