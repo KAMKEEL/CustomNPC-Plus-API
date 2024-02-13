68047a7f595648f060b16c4526b5df97340b3846
Kam <KamronGaming@gmail.com>
2024-02-13 14:38:08 -0500

    Untest haha!

--- java/net/minecraft/block/state/IBlockState.java
+++ java/net/minecraft/block/state/IBlockState.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 import net.minecraft.block.Block;
 import net.minecraft.block.properties.IProperty;
 
-/** This code is owned by Minecrafdt */
+/** This code is owned by Minecraft */
 
 public interface IBlockState
 {
