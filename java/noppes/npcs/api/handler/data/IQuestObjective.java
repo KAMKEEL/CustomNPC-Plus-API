5d059aea800f572b7268ac679310ebc8b3d13f6c
Kam <KamronGaming@gmail.com>
2024-03-19 05:08:01 -0400

    setProgress Comments and new setPlayerProgress for Quest Objective All

--- java/noppes/npcs/api/handler/data/IQuestObjective.java
+++ java/noppes/npcs/api/handler/data/IQuestObjective.java
@@ -3,7 +3,26 @@ package noppes.npcs.api.handler.data;
 public interface IQuestObjective {
     int getProgress();
 
-    void setProgress(int var1);
+    /**
+     * If this function is used on a player within a party and the
+     * quest objective type is set to ALL. It will set everyone to have
+     * the same progress within the party.
+     *
+     * @param progress Progress Amount
+     */
+    void setProgress(int progress);
+
+    /**
+     * This function is typically used for setting individual
+     * progress for a player in a party for a Quest Objective
+     * that is set for ALL Players. This function will work
+     * like the normal setProgress if the player does not
+     * have a party
+     *
+     * @param playerName Name of Player
+     * @param progress Progress Amount
+     */
+    void setPlayerProgress(String playerName, int progress);
 
     int getMaxProgress();
 
