f91f9b43ba47a6bc1f6970537792b3b8ce49f4b8
Kam <KamronGaming@gmail.com>
2025-02-10 02:44:13 -0500

    Anvil Recipes

--- java/noppes/npcs/api/handler/IRecipeHandler.java
+++ java/noppes/npcs/api/handler/IRecipeHandler.java
@@ -1,6 +1,7 @@
 package noppes.npcs.api.handler;
 
 import net.minecraft.item.ItemStack;
+import noppes.npcs.api.handler.data.IAnvilRecipe;
 import noppes.npcs.api.handler.data.IRecipe;
 
 import java.util.List;
@@ -10,9 +11,15 @@ public interface IRecipeHandler {
 
     List<IRecipe> getCarpentryList();
 
+    List<IAnvilRecipe> getAnvilList();
+
     void addRecipe(String var1, boolean var2, ItemStack var3, Object... var4);
 
     void addRecipe(String var1, boolean var2, ItemStack var3, int var4, int var5, ItemStack... var6);
 
     IRecipe delete(int var1);
+
+    IAnvilRecipe deleteAnvil(int var1);
+
+    void addAnvilRecipe(String name, boolean global, ItemStack itemToRepair, ItemStack repairMaterial, int xpCost, float repairPercentage);
 }
