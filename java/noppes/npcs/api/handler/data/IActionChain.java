40f33b264686e9fbb8401f30411a6cfe29211893
bigguy345 <monkeybar3200@gmail.com>
2025-04-29 09:00:17 +0300

    Added IActionChain after(int delay, String name, Consumer<IAction> task) to IActionChain

--- java/noppes/npcs/api/handler/data/IActionChain.java
+++ java/noppes/npcs/api/handler/data/IActionChain.java
@@ -4,5 +4,7 @@ import java.util.function.Consumer;
 
 public interface IActionChain {
 
+    IActionChain after(int delay, String name, Consumer<IAction> task);
+
     IActionChain after(int delay, Consumer<IAction> task);
 }
