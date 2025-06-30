13d32ae42518a70ea3cc80677a7d3e611a2aef9b
bigguy345 <monkeybar3200@gmail.com>
2025-06-30 16:37:40 +0300

    Adjusted ActionChains

--- java/noppes/npcs/api/handler/data/IActionChain.java
+++ java/noppes/npcs/api/handler/data/IActionChain.java
@@ -7,4 +7,10 @@ public interface IActionChain {
     IActionChain after(int delay, String name, Consumer<IAction> task);
 
     IActionChain after(int delay, Consumer<IAction> task);
+
+    /**
+     * Start IActionManager
+     * @return
+     */
+    IActionChain start();
 }
