20e07bfc5025d5bb613180ea03879b154fcf0a79
Kam <KamronGaming@gmail.com>
2025-07-08 14:38:41 -0400

    NEED TO COmmit this FOR NOW sorry GOATEE!!!!

--- java/noppes/npcs/api/handler/data/IActionListener.java
+++ java/noppes/npcs/api/handler/data/IActionListener.java
@@ -1,11 +1,9 @@
 package noppes.npcs.api.handler.data;
 
-import noppes.npcs.controllers.data.action.ActionListener;
-
 public interface IActionListener {
-    ActionListener.Hook getOrCreateHook(String name);
-
-    ActionListener.Hook getHook(String hookName);
-
-    boolean fire(String name);
+//    ActionListener.Hook getOrCreateHook(String name);
+//
+//    ActionListener.Hook getHook(String hookName);
+//
+//    boolean fire(String name);
 }
