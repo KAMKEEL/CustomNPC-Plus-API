a58b6383aaff639777d7389ccc81083bf179292d
Kam <KamronGaming@gmail.com>
2025-07-08 16:34:28 -0400

    IActionListener Comments

--- java/noppes/npcs/api/handler/data/IActionListener.java
+++ java/noppes/npcs/api/handler/data/IActionListener.java
@@ -1,6 +1,7 @@
 package noppes.npcs.api.handler.data;
 
 public interface IActionListener {
+    // IActionListener Comments
 //    ActionListener.Hook getOrCreateHook(String name);
 //
 //    ActionListener.Hook getHook(String hookName);
