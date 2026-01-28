b8ba682e9dd4e542a5ccf0de231ab5e5ebdad448
Kam <KamronGaming@gmail.com>
2026-01-28 16:31:00 -0500

    Update IAnimationHandler.java

--- java/noppes/npcs/api/handler/IAnimationHandler.java
+++ java/noppes/npcs/api/handler/IAnimationHandler.java
@@ -16,4 +16,28 @@ public interface IAnimationHandler {
     IAnimation get(int id);
 
     IAnimation[] getAnimations();
+
+    /**
+     * Get all built-in animations.
+     * Built-in animations are read-only and cannot be modified or deleted.
+     */
+    IAnimation[] getBuiltInAnimations();
+
+    /**
+     * Get all animations (both built-in and user-created).
+     */
+    IAnimation[] getAllAnimations();
+
+    /**
+     * Check if an animation name is a built-in animation.
+     *
+     * @param name The animation name to check
+     * @return true if this is a built-in animation
+     */
+    boolean isBuiltIn(String name);
+
+    /**
+     * Get names of all built-in animations.
+     */
+    String[] getBuiltInAnimationNames();
 }
