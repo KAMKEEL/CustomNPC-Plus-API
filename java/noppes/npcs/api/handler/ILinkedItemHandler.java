253ef7329814f37578a771363e4bde0b61bc6fcd
Vidal <imnotputtingmyemailhereami@gmail.com>
2026-02-24 20:23:43 -0300

    Linked Item API methods

---
+++ java/noppes/npcs/api/handler/ILinkedItemHandler.java
@@ -0,0 +1,21 @@
+package noppes.npcs.api.handler;
+
+import noppes.npcs.api.handler.data.ILinkedItem;
+import noppes.npcs.api.item.IItemStack;
+import noppes.npcs.controllers.data.LinkedItem;
+
+public interface ILinkedItemHandler {
+    ILinkedItem createItem(String name);
+
+    IItemStack createItemStack(int id);
+
+    void add(ILinkedItem linkedItem);
+
+    ILinkedItem remove(int id);
+
+    ILinkedItem get(int id);
+
+    boolean contains(int id);
+
+    boolean contains(ILinkedItem linkedItem);
+}
