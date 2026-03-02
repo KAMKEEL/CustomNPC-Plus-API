9fbd1eceffaa32f088676ff89d13fe3ca4ba1155
Kam <KamronGaming@gmail.com>
2026-03-02 15:23:53 -0500

    Small Bug Fixes to Flight, Command Handling, Linked Items

--- java/noppes/npcs/api/item/IItemLinked.java
+++ java/noppes/npcs/api/item/IItemLinked.java
@@ -6,4 +6,12 @@ public interface IItemLinked extends IItemCustomizable {
 
     public ILinkedItem getLinkedItem();
 
+    /**
+     * Sets the current durability value for this linked item stack.
+     * This is a per-stack override, not changing the linked item template.
+     *
+     * @param durabilityValue The durability value
+     */
+    void setDurabilityValue(float durabilityValue);
+
 }
